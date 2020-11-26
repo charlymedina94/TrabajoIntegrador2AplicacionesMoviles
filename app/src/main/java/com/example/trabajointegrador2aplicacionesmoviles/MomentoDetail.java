f132355871327b8fbef9a2c5d024537f130512bb
Carlos Medina <mcarlosezequiel@gmail.com>
2020-11-26 11:18:32 -0300

    agrego logica para que muestre la imagen en el detalle

--- app/src/main/java/com/example/trabajointegrador2aplicacionesmoviles/MomentoDetail.java
+++ app/src/main/java/com/example/trabajointegrador2aplicacionesmoviles/MomentoDetail.java
@@ -1,23 +1,12 @@
 package com.example.trabajointegrador2aplicacionesmoviles;
 
-import android.app.Activity;
-import android.app.AlertDialog;
-import android.app.Dialog;
-import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.media.Image;
 import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -31,7 +20,6 @@ import com.example.trabajointegrador2aplicacionesmoviles.entidades.Momento;
 
 import java.io.FileNotFoundException;
 import java.io.InputStream;
-import java.util.ArrayList;
 
 
 public class MomentoDetail extends AppCompatActivity {
@@ -47,12 +35,13 @@ public class MomentoDetail extends AppCompatActivity {
     TextView ubicacionn;
 
     ImageView imgFragment;
-
+    public static ConexionSQLiteHelper sqLiteHelper;
 
 
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.momento_items_fragment);
+        sqLiteHelper = new ConexionSQLiteHelper(this, "MomentoDB.sqlite", null, 1);
 
         descripcionn = (TextView) findViewById(R.id.txtDescripcionFragment);
         fechaa = (TextView) findViewById(R.id.txtFecha);
@@ -68,7 +57,8 @@ public class MomentoDetail extends AppCompatActivity {
             cursor.moveToPosition(position);
             int id = cursor.getInt(0);
             String descripcion = cursor.getString(1);
-            byte[] image = cursor.getBlob(2);
+//            byte[] image = cursor.getBlob(2);
+            imgFragment.setImageBitmap(getImageFromBLOB(cursor.getBlob(cursor.getColumnIndex("image"))));;
             String fecha = cursor.getString(3);
             String ubicacion = cursor.getString(4);
 
@@ -76,11 +66,51 @@ public class MomentoDetail extends AppCompatActivity {
             fechaa.setText(fecha);
             ubicacionn.setText(ubicacion);
             //imgFragment.setImageBitmap(image);
-
             //new Momento(descripcion, image, id, fecha, ubicacion);
 
 
 
         }
+
+    public static Bitmap getImageFromBLOB(byte[] mBlob)
+    {
+        byte[] bb = mBlob;
+        return BitmapFactory.decodeByteArray(bb, 0, bb.length);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        if(requestCode == 888){
+            if(grantResults.length >0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(intent, 888);
+            }
+            else {
+                Toast.makeText(getApplicationContext(), "No tenes permisos para acceder a la ubicación del archivo!", Toast.LENGTH_SHORT).show();
+            }
+            return;
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if(requestCode == 888 && resultCode == RESULT_OK && data != null){
+            Uri uri = data.getData();
+            try {
+                InputStream inputStream = getContentResolver().openInputStream(uri);
+                Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                //imageViewFood.setImageBitmap(bitmap);
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
 
     }
